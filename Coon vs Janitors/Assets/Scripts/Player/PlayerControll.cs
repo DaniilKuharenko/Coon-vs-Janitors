5bcc4767f129d21cb160d533d71ceb63166bd38b
Cyber Duck <ladanielworkua@gmail.com>
2024-12-19 22:16:02 +0200

    Player controll commit

--- Coon vs Janitors/Assets/Scripts/Player/PlayerControll.cs
+++ Coon vs Janitors/Assets/Scripts/Player/PlayerControll.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Raccons_House_Games
 {
-    public class PlayerControll : MonoBehaviour
+    public class PlayerControll : Player
     {
-        // Start is called before the first frame update
-        void Start()
+        [SerializeField] private Rigidbody _playerBody;
+        [SerializeField] private FixedJoystick _movementJoystick;
+        private Player _player;
+        private Vector2 _currentInput;
+        private float _checkSpeed;
+
+        private void Update()
         {
-        
+            HandleMove();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void HandleMove()
         {
-        
+            _currentInput = new Vector2(_movementJoystick.Horizontal, _movementJoystick.Vertical);
+            Vector3 movement = new Vector3(_currentInput.x * 10.0f, _playerBody.velocity.y, _currentInput.y * 10.0f);
+
+            _playerBody.velocity = movement;
+            _checkSpeed = new Vector3(_playerBody.velocity.x, 0, _playerBody.velocity.z).magnitude;
+
+            if(_currentInput != Vector2.zero)
+            {
+                transform.rotation = Quaternion.LookRotation(new Vector3(_playerBody.velocity.x, 0, _playerBody.velocity.z));
+            }
         }
     }
 }

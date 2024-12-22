492c4af409b1fefa3227df5ceebf2f97483a76a9
Cyber Duck <ladanielworkua@gmail.com>
2024-12-22 20:39:42 +0200

    Update GameManager.cs

--- Coon vs Janitors/Assets/Scripts/Managers/GameManager.cs
+++ Coon vs Janitors/Assets/Scripts/Managers/GameManager.cs
@@ -4,6 +4,15 @@ namespace Raccons_House_Games
 {
     public class GameManager : MonoBehaviour
     {
-        
+        private StateMachine _playerStateMachine;
+        private StateMachine _enemyStateMAchine;
+
+        private PlayerControll _player;
+        private EnemyControll _enemy;
+
+        private void StartMachine()
+        {
+            
+        }
     }
 }

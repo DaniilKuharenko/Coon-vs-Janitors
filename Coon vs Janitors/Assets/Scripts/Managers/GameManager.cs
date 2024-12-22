5d71522dd6936f0859945bfcfd4c9e2bdebfb36c
Cyber Duck <ladanielworkua@gmail.com>
2024-12-23 00:46:01 +0200

    Update GameManager.cs

--- Coon vs Janitors/Assets/Scripts/Managers/GameManager.cs
+++ Coon vs Janitors/Assets/Scripts/Managers/GameManager.cs
@@ -1,18 +1,60 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Raccons_House_Games
 {
     public class GameManager : MonoBehaviour
     {
-        private StateMachine _playerStateMachine;
-        private StateMachine _enemyStateMAchine;
+        [SerializeField] private ReferenceHolder _referenceHolder;
+        [SerializeField] private Transform _playerSpawnPoint;
+        [SerializeField] private Transform[] _enemySpawnPoints;
+        [SerializeField] private Transform _poolParent;
 
-        private PlayerControll _player;
-        private EnemyControll _enemy;
+        private ObjectPool _playerPool;
+        private List<ObjectPool> _enemyPools;
+        private GameObject _playerInstance;
 
-        private void StartMachine()
+        public void Start()
         {
-            
+            InitializePools();
+            SpawnPlayer();
+            SpawnEnemies();
+        }
+
+        private void InitializePools()
+        {
+            // Create a pool for the player
+            _playerPool = new ObjectPool(_referenceHolder.PlayerPrefab.gameObject, _poolParent, 1);
+
+            // Create pools for the enemies
+            _enemyPools = new List<ObjectPool>();
+            foreach(var enemyPrefab in _referenceHolder.EnemyPrefabs)
+            {
+                var pool = new ObjectPool(enemyPrefab.gameObject, _poolParent, 5);
+                _enemyPools.Add(pool);
+            }
+        }
+
+        private void SpawnPlayer()
+        {
+            // Get a player from the pool
+            _playerInstance = _playerPool.GetFromPool();
+            _playerInstance.transform.position = _playerSpawnPoint.position;
+
+            // Initialize the player's state machine
+            // var playerControll = _playerInstance.GetComponent<PlayerControll>();
+            // playerControl.InitializeStateMachine();
+        }
+
+        private void SpawnEnemies()
+        {
+            // For each spawn point, select a random pool of enemies and spawn them
+            foreach(var spawnPoint in _enemySpawnPoints)
+            {
+                var pool = _enemyPools[Random.Range(0, _enemyPools.Count)];
+                var enemyInstance = pool.GetFromPool();
+                enemyInstance.transform.position = spawnPoint.position;
+            }
         }
     }
 }

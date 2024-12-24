f8c8a22ee2a9b6873b6dad4e9422492add0408f2
Cyber Duck <ladanielworkua@gmail.com>
2024-12-24 20:56:52 +0200

    Update SnowController.cs

--- Coon vs Janitors/Assets/Scripts/Controllers/SnowController.cs
+++ Coon vs Janitors/Assets/Scripts/Controllers/SnowController.cs
@@ -1,21 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Raccons_House_Games
 {
     public class SnowController : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+        [SerializeField] private ComputeShader _snowComputerShader;
+        [SerializeField] private RenderTexture _snowRT;
+        [SerializeField] private float _colorValueToAdd;
+        [SerializeField] private int _resolution = 512;
+        [SerializeField] private float _spotSize = 10;
+        [SerializeField] private Vector2Int _postion = new Vector2Int(256, 256);
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+        private string _snowImageProperty = "snowImage";
+        private string _colorValueProperty = "colorValueToAdd";
+        private string _resolutionProperty = "resolution";
+        private string _positionXProperty = "positionX";
+        private string _positionYProperty = "positionY";
+        private string _spotSizeProperty = "spotSize";
+
+        private string _csMainKernel = "";
+        private string _fillWhiteKernel = "";
+
+        private MeshRenderer _meshRenderer;
+
     }
 }

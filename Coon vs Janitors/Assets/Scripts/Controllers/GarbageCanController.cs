43cff215466dab6a5439160ca25b24cb7de0b32c
Cyber Duck <ladanielworkua@gmail.com>
2024-12-28 20:12:19 +0200

    GarbageCanController commit

--- Coon vs Janitors/Assets/Scripts/Controllers/GarbageCanController.cs
+++ Coon vs Janitors/Assets/Scripts/Controllers/GarbageCanController.cs
@@ -1,9 +1,73 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Raccons_House_Games
 {
     public class GarbageCanController : MonoBehaviour
     {
-        
+        [SerializeField] private float _interactionTime = 2f; // Button hold time (in seconds)
+        private List<GameObject> _trashInCan;
+        private TrashController _trashController; 
+        private bool _isPlayerInZone = false;
+        private bool _isInteracting = false;
+
+        public void InitializeTrash(List<GameObject> trash, TrashController trashController)
+        {
+            _trashInCan = trash;
+            _trashController = trashController;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _isPlayerInZone = true;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _isPlayerInZone = false;
+                _isInteracting = false; // Abort the interaction
+            }
+        }
+
+        private void Update()
+        {
+            if (_isPlayerInZone && Input.GetKey(KeyCode.E)) // Test button
+            {
+                if (!_isInteracting)
+                {
+                    StartInteraction();
+                }
+            }
+            else if (_isInteracting && Input.GetKeyUp(KeyCode.E))
+            {
+                _isInteracting = false; // Interrupt the interaction if the button is released
+            }
+        }
+
+        private async void StartInteraction()
+        {
+            _isInteracting = true;
+
+            // wait time to end
+            await Task.Delay((int)(_interactionTime * 1000));
+
+            // Check if the player held the button all the time
+            if (_isInteracting && _trashInCan.Count > 0)
+            {
+                // Let the garbage out to walking Lmfao
+                GameObject trash = _trashInCan[0];
+                _trashInCan.RemoveAt(0);
+                trash.transform.position = transform.position + Vector3.up;
+                trash.SetActive(true);
+            }
+
+            _isInteracting = false;
+        }
     }
 }
--- Coon vs Janitors/Assets/Scripts/Controllers/TrashController.cs
+++ Coon vs Janitors/Assets/Scripts/Controllers/TrashController.cs
@@ -5,32 +5,54 @@ namespace Raccons_House_Games
 {
     public class TrashController : MonoBehaviour
     {
+        [SerializeField] private GameObject _garbageCanPrefab;
         [SerializeField] private GameTrash _trashData;
         [SerializeField] private Vector2 _spawnArea = new Vector2(10f, 10f);
-        [SerializeField] private int _numberOfTrashToSpawn = 5;
+        [SerializeField] private int _numberOfTrashToSpawn = 10; // Total amount of garbage
+        [SerializeField] private int _numberOfGarbageCans = 5;
+        [SerializeField] private int _trashPerCan = 5; // Trash to the tank
+
         private readonly List<GameObject> _activeTrash = new List<GameObject>();
+        private readonly List<GameObject> _activeGarbageCans = new List<GameObject>();
         private readonly List<ObjectPool> _trashPools = new List<ObjectPool>();
+        private ObjectPool _garbageCanPool;
 
         private void Start()
         {
+            // Initialization of trash pools
             foreach (var prefab in _trashData.TrashPrefabs)
             {
-                var pool = new ObjectPool(prefab, transform, 15);
+                var pool = new ObjectPool(prefab, transform, 65);
                 _trashPools.Add(pool);
             }
 
+            // Initialization of the garbage pool
+            _garbageCanPool = new ObjectPool(_garbageCanPrefab, transform, _numberOfGarbageCans);
+
             SpawnTrash();
+            SpawnGarbageCans();
         }
 
         private void SpawnTrash()
         {
-            for (int i = 0; i < _numberOfTrashToSpawn; i++)
-            {
-                int randomPoolIndex = Random.Range(0, _trashPools.Count);
-                ObjectPool selectedPool = _trashPools[randomPoolIndex];
+            // Half of the garbage is spawned as separate objects
+            int trashToSpawnIndividually = _numberOfTrashToSpawn / 2;
 
-                GameObject trash = selectedPool.GetFromPool();
-                if (trash != null)
+            for (int i = 0; i < trashToSpawnIndividually; i++)
+            {
+                SpawnTrashAtRandomPosition();
+            }
+        }
+
+        private void SpawnGarbageCans()
+        {
+            int remainingTrash = _numberOfTrashToSpawn / 2;
+            int trashAssigned = 0;
+
+            for (int i = 0; i < _numberOfGarbageCans; i++)
+            {
+                GameObject garbageCan = _garbageCanPool.GetFromPool();
+                if (garbageCan != null)
                 {
                     Vector3 randomPosition = new Vector3(
                         Random.Range(-_spawnArea.x / 2f, _spawnArea.x / 2f),
@@ -38,14 +60,62 @@ namespace Raccons_House_Games
                         Random.Range(-_spawnArea.y / 2f, _spawnArea.y / 2f)
                     );
 
-                    trash.transform.position = transform.position + randomPosition;
-                    _activeTrash.Add(trash);
+                    garbageCan.transform.position = transform.position + randomPosition;
+                    garbageCan.SetActive(true);
+
+                    _activeGarbageCans.Add(garbageCan); // Add the tank to the list of active tanks
+
+                    // Distribution of trash into the tank
+                    var canController = garbageCan.GetComponent<GarbageCanController>();
+                    if (canController != null)
+                    {
+                        List<GameObject> trashForCan = new List<GameObject>();
+
+                        for (int j = 0; j < _trashPerCan && trashAssigned < remainingTrash; j++)
+                        {
+                            GameObject trash = SpawnTrashAtRandomPosition(false); // Получаем мусор
+                            if (trash != null)
+                            {
+                                trashForCan.Add(trash);
+                                trashAssigned++;
+                            }
+                        }
+
+                        // Transferring the trash to the tank
+                        canController.InitializeTrash(trashForCan, this);
+                    }
                 }
             }
         }
 
+        private GameObject SpawnTrashAtRandomPosition(bool activate = true)
+        {
+            int randomPoolIndex = Random.Range(0, _trashPools.Count);
+            ObjectPool selectedPool = _trashPools[randomPoolIndex];
+
+            GameObject trash = selectedPool.GetFromPool();
+            if (trash != null)
+            {
+                Vector3 randomPosition = new Vector3(
+                    Random.Range(-_spawnArea.x / 2f, _spawnArea.x / 2f),
+                    0.5f, // height
+                    Random.Range(-_spawnArea.y / 2f, _spawnArea.y / 2f)
+                );
+
+                trash.transform.position = transform.position + randomPosition;
+                trash.SetActive(activate);
+                if (activate)
+                {
+                    _activeTrash.Add(trash);
+                }
+            }
+
+            return trash;
+        }
+
         public void ClearTrash()
         {
+            // Deactivating the trash
             foreach (var trash in _activeTrash)
             {
                 trash.SetActive(false);
@@ -55,6 +125,14 @@ namespace Raccons_House_Games
                 }
             }
             _activeTrash.Clear();
+
+            // Deactivating the trash tank
+            foreach (var garbageCan in _activeGarbageCans)
+            {
+                garbageCan.SetActive(false);
+                _garbageCanPool.ReturnToPool(garbageCan);
+            }
+            _activeGarbageCans.Clear();
         }
 
         public void ReturnTrashToPool(GameObject trash)
@@ -72,6 +150,5 @@ namespace Raccons_House_Games
                 }
             }
         }
-
     }
 }

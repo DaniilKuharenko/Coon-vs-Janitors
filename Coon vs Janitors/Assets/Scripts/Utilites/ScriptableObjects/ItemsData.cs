59a31f823db59645d3f544706114eae08ae54a2a
Cyber Duck <ladanielworkua@gmail.com>
2024-12-28 18:11:02 +0200

    ItemsData commit

---
+++ Coon vs Janitors/Assets/Scripts/Utilites/ScriptableObjects/ItemsData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Raccons_House_Games
+{
+    [CreateAssetMenu(fileName = "NewItemsData", menuName = "CoonGame/Items")]
+    public class ItemsData : ScriptableObject
+    {
+        
+    }
+}

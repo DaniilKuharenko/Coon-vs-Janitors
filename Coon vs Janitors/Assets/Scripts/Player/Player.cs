9acac0766f5b1f0e4d60d9e699a09815da9393e6
Cyber Duck <ladanielworkua@gmail.com>
2025-01-06 16:48:47 +0200

    new commit

--- Coon vs Janitors/Assets/Scripts/Player/Player.cs
+++ Coon vs Janitors/Assets/Scripts/Player/Player.cs
@@ -4,11 +4,5 @@ namespace Raccons_House_Games
 {
     public class Player : MonoBehaviour
     {
-        [SerializeField] private ClawedGloves _clawedGloves;
-
-        private void Awake()
-        {
-            _clawedGloves.InitializeGloves();
-        }
     }
 }

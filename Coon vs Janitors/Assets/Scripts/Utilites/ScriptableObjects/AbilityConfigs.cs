249d18a46b5f1aec6d93e251b50a5340b3939b23
Cyber Duck <ladanielworkua@gmail.com>
2025-01-06 22:14:23 +0200

    Ability commit

--- Coon vs Janitors/Assets/Scripts/Utilites/ScriptableObjects/AbilityConfigs.cs
+++ Coon vs Janitors/Assets/Scripts/Utilites/ScriptableObjects/AbilityConfigs.cs
@@ -7,6 +7,7 @@ namespace Raccons_House_Games
         [field: SerializeField] public string Title { get; private set; }
         [field: SerializeField] public string Description { get; private set; }
         [field: SerializeField] public Sprite DisplayImage { get; private set; }
+        [field: SerializeField] public float DurationTime { get; private set; }
 
         public virtual AbilityBuilder GetBuilder() => new AbilityBuilder(this);
 

5f1189a5acdfd6eb7822ae935903423f52f64d29
Cyber Duck <ladanielworkua@gmail.com>
2025-01-07 21:47:49 +0200

    Update CoffeemugAbility.cs

--- Coon vs Janitors/Assets/Scripts/ItemsAbility/Items/Coffeemug/CoffeemugAbility.cs
+++ Coon vs Janitors/Assets/Scripts/ItemsAbility/Items/Coffeemug/CoffeemugAbility.cs
@@ -4,5 +4,11 @@ namespace Raccons_House_Games
 {
     public class CoffeemugAbility : AbilityItem
     {
+        public float SpeedBoost { get; private set; }
+
+        public CoffeemugAbility(float speedBoost)
+        {
+            SpeedBoost = speedBoost;
+        }
     }
 }

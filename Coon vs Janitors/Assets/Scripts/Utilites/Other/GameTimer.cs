c6bb3a61a14dde273fe3f3a912564f5b13b976f0
Cyber Duck <ladanielworkua@gmail.com>
2025-01-12 18:56:31 +0200

    Update GameTimer.cs

--- Coon vs Janitors/Assets/Scripts/Utilites/Other/GameTimer.cs
+++ Coon vs Janitors/Assets/Scripts/Utilites/Other/GameTimer.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections;
 
 namespace Raccons_House_Games
 {
@@ -10,15 +11,42 @@ namespace Raccons_House_Games
         [SerializeField] private TMP_Text _timerText;
         [SerializeField] private int _duration;
         private int _remainingDuration;
+        private bool _pause;
 
         private void Start() // temporary use of Start later it will need to be removed
         {
-
+            Begin(_duration);
         }
 
         private void Begin(int second)
         {
+            _remainingDuration = second;
+            StartCoroutine(TimerTick());
+        }
 
+        private IEnumerator TimerTick()
+        {
+            while(_remainingDuration >= 0)
+            {
+                if(!_pause)
+                {
+                    _timerText.text = $"{_remainingDuration / 60:00} : {_remainingDuration % 60:00}";
+                    _circleFill.fillAmount = Mathf.InverseLerp(0, _duration, _remainingDuration);
+                    yield return new WaitForSeconds(1f);
+                }
+                yield return null;
+            }
+            OnEndTime();
+        }
+
+        private void OnEndTime()
+        {
+            Debug.Log("Timer End");
+        }
+
+        public void Pause()
+        {
+            _pause = !_pause;
         }
     }
 }

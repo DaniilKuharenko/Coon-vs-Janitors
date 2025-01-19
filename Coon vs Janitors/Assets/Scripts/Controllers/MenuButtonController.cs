453a5f8dc5ed2d681c065b6531160e6b7a9ac950
Cyber Duck <ladanielworkua@gmail.com>
2025-01-19 16:21:22 +0200

    Menu UI commit

---
+++ Coon vs Janitors/Assets/Scripts/Controllers/MenuButtonController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Raccons_House_Games
+{
+    public class MenuButtonController : MonoBehaviour
+    {
+        [SerializeField] private Button _playButton;
+        [SerializeField] private Button _settingsButton;
+        [SerializeField] private Button _shopButton;
+        
+        private void Awake()
+        {
+            _playButton.onClick.AddListener(OnPlayButtonClicked);
+            _settingsButton.onClick.AddListener(OnSettingsButtonClicked);
+            _shopButton.onClick.AddListener(OnShopButtonClicked);
+        }
+
+        private void OnPlayButtonClicked()
+        {
+            Debug.Log("Play button was clicked!");
+        }
+
+        private void OnSettingsButtonClicked()
+        {
+            Debug.Log("Settings button was clicked!");
+        }
+
+        private void OnShopButtonClicked()
+        {
+            Debug.Log("Shop button was clicked!");
+        }
+    }
+}
